3ea41471b09132422f72d9ada38d2003e8697b7a
Anderson Siqueira <contato@andersonls.com.br>
2026-03-31 00:39:13 -0300

    Aula 17

---
+++ modulo-condicionais/src/tech/buildrun/kickstart/aula17/Main.java
@@ -0,0 +1,23 @@
+package tech.buildrun.kickstart.aula17;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int opcao = 2;
+
+        switch (opcao){
+
+            case 1:
+                System.out.println("Opção com valor 1");
+                break;
+            case 2:
+                System.out.println("Opção com valor 2");
+                break;
+            default:
+                System.out.println("Fluxo Padrão! Nenhuma opção de valor!");
+                break;
+
+        }
+
+    }
+}
