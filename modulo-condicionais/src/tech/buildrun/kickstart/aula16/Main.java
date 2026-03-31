84500b3694446aa4c88eebc2bf3560bf81edad9f
Anderson Siqueira <contato@andersonls.com.br>
2026-03-30 23:01:59 -0300

    Aula 16

---
+++ modulo-condicionais/src/tech/buildrun/kickstart/aula16/Main.java
@@ -0,0 +1,10 @@
+package tech.buildrun.kickstart.aula16;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean resultado = true;
+
+        System.out.println(!resultado);
+    }
+}
