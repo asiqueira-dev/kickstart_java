91472fc4b8a5efe33d27f050025de8407bce74cb
Anderson Siqueira <contato@andersonls.com.br>
2026-03-30 22:37:50 -0300

    aula 15

---
+++ modulo-condicionais/src/tech/buildrun/kickstart/aula15/Main.java
@@ -0,0 +1,16 @@
+package tech.buildrun.kickstart.aula15;
+
+public class Main {
+    public static void main(String[] args) {
+
+         boolean isFinalSemana = false;
+         boolean isFeriado = false;
+
+         if(isFinalSemana || isFeriado){
+             System.out.println("Eh!, dia de descansar!");
+         }else {
+             System.out.println("Eh!, dia de trabalhar");
+
+         }
+    }
+}
