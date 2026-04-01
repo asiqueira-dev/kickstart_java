61cedeac38185f491a51280ef904f7b4f4b7c25c
Anderson Siqueira <contato@andersonls.com.br>
2026-03-31 22:47:32 -0300

    exercicio Aula 17

---
+++ modulo-condicionais/src/tech/buildrun/kickstart/aula17/Exercicio.java
@@ -0,0 +1,49 @@
+package tech.buildrun.kickstart.aula17;
+
+import java.util.Scanner;
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+        System.out.println("*** Tela de estação do Ano ***");
+
+        System.out.println("Digite o mÊs atual: ");
+        Scanner sc = new Scanner(System.in);
+
+        int mes = sc.nextInt();
+
+        switch (mes){
+
+
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Verão!");
+                break;
+
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Outono!");
+                break;
+
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Inverno!");
+                break;
+
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Primavera!");
+                break;
+        }
+
+
+
+
+
+
+    }
+}
